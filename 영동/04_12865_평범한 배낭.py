d3900e48fabc38055b1346d0748543ce97777e75
zeroistfilm <zeroistfilm@naver.com>
2021-01-04 09:29:53 +0900

    solved

--- 영동/04_12865_평범한 배낭.py
+++ 영동/04_12865_평범한 배낭.py
@@ -1,72 +1,23 @@
 # https://www.acmicpc.net/problem/12865
 
 import sys
-from collections import deque
-combi = [[-1,[]] for i in range(100001)]
+N, K  = map(int, sys.stdin.readline().split())
+items = [list(map(int, sys.stdin.readline().split())) for i in range(N)]
 
-N,K = map(int,sys.stdin.readline().split())
-objects = sorted(list(map(int, sys.stdin.readline().split())) for i in range(N))
-
-queue = deque(objects)
-tmp=deque([])
-
-for i in range(len(objects)):
-    combi[objects[i][0]][0] = objects[i][1] #value
-    combi[objects[i][0]][1].append(objects[i][0]) #objects
-
-while True:
-    item = queue.popleft()
-    for other in queue:
-
-
-        breakflag=False
-        #중복된 조합을 체크한다.
-        overlap=[]
-        for i in range(len(combi[item[0]][1])):  # 아이템 조합을 저장해놓는다.
-            overlap.append(combi[item[0]][1][i])
-        for i in range(len(combi[other[0]][1])):
-            overlap.append(combi[other[0]][1][i])
-        numvisit = [0 for i in range(101)] #중복된 숫자 체크
-        for i in overlap:
-            numvisit[i]+=1
-        numcheck=[x for x in numvisit if x != 0] #0은 제거후 나머지만 반환
-        for i in numcheck:
-            if i != 1:
-                breakflag=True
-        if breakflag ==True: #
-            continue
+dp = [[0 for i in range(K+1)]for i in range(len(items)+1)]
 
 
 
+for c in range(K+1):
+    for r in range(1,len(items)+1):
+        limitW = c
+        weight = items[r-1][0]
+        value = items[r-1][1]
 
-        if combi[item[0] + other[0]][0] == -1:#조합에 값이 없으면 조합 value를 넣는다
-            combi[item[0] + other[0]][0] = item[1]+other[1] # values
+        if weight > limitW:
+            dp[r][c] = dp[r-1][c]
+        else:
+            dp[r][c] = max(dp[r-1][c], dp[r-1][c-weight]+value)
 
-            for i in range(len(combi[item[0]][1])): #아이템 조합을 저장해놓는다.
-                combi[item[0] + other[0]][1].append(combi[item[0]][1][i])
-            for i in range(len(combi[other[0]][1])):
-                combi[item[0] + other[0]][1].append(combi[other[0]][1][i])
 
-        else: #이미 들어있는 값이면 비교해야함.
-            if item[1] + other[1]>combi[item[0] + other[0]][0]:# 기존에 있던 것이더 작으면
-                combi[item[0] + other[0]][0] = item[1] + other[1] #value 값을 업데이트 하고
-                combi[item[0] + other[0]][1]=[]#리스트를 교환한다.
-                for i in range(len(combi[item[0]][1])):
-                    combi[item[0] + other[0]][1].append(combi[item[0]][1][i])
-                for i in range(len(combi[other[0]][1])):
-                    combi[item[0] + other[0]][1].append(combi[other[0]][1][i])
-        tmp.append([item[0]+other[0],item[1]+other[1]])
-
-    for _ in range(len(tmp)):
-        queue.append(tmp.popleft())
-
-    # 종료 체크, 모든 아이템을 사용한 경우를 반환한다.
-    endflag = False
-    endcheck = [x for x in combi if x[0] != -1]
-    for i in endcheck:
-        if len(i[1]) == N:
-            endflag = True
-    if endflag == True:
-        break
-
-print(combi[K][0])
+print(dp[-1][-1])
---
+++ 영동/08_11047_동전 0.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/11047
+
+import sys
+N, K = map(int, sys.stdin.readline().split())
+coins=[]
+for i in range(N):
+    coin = int(sys.stdin.readline())
+    if coin <=K:
+        coins.append(coin)
+count=0
+tmp=K
+for coin in coins[::-1]:
+    count+=K//coin
+    K=K%coin
+    if K==0:
+        break
+print(count)
